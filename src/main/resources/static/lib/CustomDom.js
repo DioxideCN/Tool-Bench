2eed5e9d314465de9e11f04fd61c4c5f6bd41adb
Dioxide_CN <dioxide-cn@qq.com>
2023-09-08 18:36:40 +0800

    修复issue#5

--- src/main/resources/static/lib/CustomDom.js
+++ src/main/resources/static/lib/CustomDom.js
@@ -151,13 +151,13 @@ function extractHeight(occupied, width, height) {
                                         </svg>
                                         ${res.forks.totalCount}
                                     </span>
-                                    <a target="_blank" href="/Team-Sangonomiya/DigitalCarbon-DataAnalysis/issues" class="mr-3">
+                                    <a target="_blank" href="${res.url}/issues" class="mr-3">
                                         <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="mr-1">
                                             <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
                                         </svg>
                                         ${res.issues.totalCount}
                                     </a>
-                                    <a target="_blank" href="/Team-Sangonomiya/DigitalCarbon-DataAnalysis/pulls" class="mr-3">
+                                    <a target="_blank" href="${res.url}/pulls" class="mr-3">
                                         <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="mr-1">
                                             <path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path>
                                         </svg>
